944028fb1d1d9be39d27945f6dc4530908b94d61
Habibaditiya21 <habibaditiya2002@gmail.com>
2022-05-11 15:18:27 +0700

    update

---
+++ SK1-Latihan 3/get_json.js
@@ -0,0 +1,36 @@
+Vue.createApp({
+  data() {
+    return {
+      header: {},
+      articles: [],
+    };
+  },
+  methods: {
+    getHeaderData() {
+      axios
+        .get("https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/SK1-Latihan%203/contents/header.json")
+        .then((res) => {
+          console.log(res.data);
+          this.header = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getArticles() {
+      axios
+        .get("https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/SK1-Latihan%203/contents/articles.json")
+        .then((res) => {
+          console.log(res.data);
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  beforeMount() {
+    this.getHeaderData();
+    this.getArticles();
+  },
+}).mount(".app");
