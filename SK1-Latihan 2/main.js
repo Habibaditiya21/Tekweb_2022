a1a3ae935d0c357e6c8cb30e288255bfa72591a3
Habibaditiya21 <habibaditiya2002@gmail.com>
2022-05-12 20:46:37 +0700

    Update file01

--- SK1-Latihan 2/main.js
+++ SK1-Latihan 2/main.js
@@ -6,28 +6,28 @@ Vue.createApp({
         nama: "Habib Aditiya Julianto",
         NIM: "NIM : 2000016047",
         description: '"Saya Adalah Mahasiswa Sistem Informasi Universitas Ahmad Dahlan Yogyakarta"',
-        imageprofile: "../img/Profil.png",
+        imageprofile: "https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/img/Profil.png",
       },
       portofolio: [
         {
           title: "Berjalan Perlahan",
           desc: "Oleh : Habib Aditiya",
-          image: "../img/Artikel01.jpg",
+          image: "https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/img/Artikel01.jpg",
         },
         {
           title: "Menjadi Manusia",
           desc: "Oleh : Habib Aditiya",
-          image: "../img/Artikel02.jpg",
+          image: "https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/img/Artikel02.jpg",
         },
         {
           title: "Tetaplah Bersama",
           desc: "Oleh : Habib Aditiya",
-          image: "../img/Artikel03.jpg",
+          image: "https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/img/Artikel03.jpg",
         },
         {
           title: "My Trip My Sleep",
           desc: "Oleh : Habib Aditiya",
-          image: "../img/Artikel04.jpg",
+          image: "https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/img/Artikel04.jpg",
         },
       ],
     };
