3fb888f26254524cb84f2916f02a6ac62915c78d
Habibaditiya21 <habibaditiya2002@gmail.com>
2022-05-12 11:04:09 +0700

    Update

---
+++ get_json.js
@@ -0,0 +1,36 @@
+Vue.createApp({
+  data() {
+    return {
+      header: {},
+      articles: [],
+    };
+  },
+  methods: {
+    getHeaderData() {
+      axios
+        .get("https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/contents/header.json")
+        .then((res) => {
+          console.log(res.data);
+          this.header = res.data;
+          this.getArticles();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getArticles() {
+      axios
+        .get("https://raw.githubusercontent.com/Habibaditiya21/tekweb2022/master/contents/articles.json")
+        .then((res) => {
+          console.log(res.data);
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  beforeMount() {
+    this.getHeaderData();
+  },
+}).mount("#app");
